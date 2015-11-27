a9518bde84b05e91ae8e7f4d7f7e30fc565793fe
Joanne Cheng <joanne@keen.io>
2015-11-26 21:09:21 -0500

    Oh god what happened

---
+++ weather_markers.js
@@ -0,0 +1,60 @@
+const WeatherMarkers = {
+  "rain/snow": L.divIcon({
+    html: '<i class="wi wi-sleet"></i>',
+    className: "weather-icon sleet"
+  }),
+
+  "rain/ice pellets": L.divIcon({
+    html: '<i class="wi wi-rain-mix"></i>',
+    className: "weather-icon sleet"
+  }),
+
+  snow: L.divIcon({
+    html: '<i class="wi wi-snow"></i>',
+    className: "weather-icon snow"
+  }),
+
+  rain: L.divIcon({
+    html: '<i class="wi wi-rain"></i>',
+    className: "weather-icon rain"
+  }),
+
+  none: L.divIcon({
+    html: '<i class="wi wi-day-sunny"></i>',
+    className: "weather-icon sunny"
+  }),
+
+  test: L.divIcon({
+    html: '<i class="wi wi-na"></i>',
+    className: "weather-icon default"
+  }),
+
+  drizzle: L.divIcon({
+    html: '<i class="wi wi-sprinkle"></i>',
+    className: "weather-icon drizzle"
+  }),
+
+  "wind-1": L.divIcon({
+    html: '<i class="wi wi-wind"></i>',
+    className: "weather-icon drizzle"
+  }),
+
+  "ice pellets": L.divIcon({
+    html: '<i class="wi wi-hail"></i>',
+    className: "weather-icon snow"
+  }),
+
+
+  default: L.divIcon({
+    html: '<i class="wi wi-moon-full"></i>',
+    className: "weather-icon default"
+  })
+}
+const weatherTypes = [
+  "hail","test","none","rain","drizzle",
+  "freezing rain","freezing drizzle","rain/snow","ice pellets/snow","rain/ice pellets",
+  "snow","wet snow","ice pellets","graupel","wind-1",
+  "wind-2","wind-3","wind-4","wind-5","flood-1",
+  "flood-2","flood-3","flood-4","mudslide","dense fog",
+  "blowing dust"
+];
